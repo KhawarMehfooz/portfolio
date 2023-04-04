0d16138826c0d16f15e14c1beed22afbaf3b43d2
Khawarmehfooz <khawarmehfooz565@gmail.com>
2023-04-04 14:15:32 +0500

    create Hero Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+class Hero extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hero Section</h1>
+      </div>
+    );
+  }
+}
+export default Hero;
