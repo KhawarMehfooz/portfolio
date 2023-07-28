ebdb238759cda280079d2fd1ef023c9091dfde42
Khawarmehfooz <khawarmehfooz565@gmail.com>
2023-07-28 14:33:08 +0500

    Update About Component

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,11 +1,23 @@
+<script>
+export default{
+    data(){
+        return{
+            subheading: "Who am I?",
+            heading: "I develop <span class='highlight__text article__heading'>quality, accessible</span> websites.",
+            description: "Hi, my name is Khawar Mehfooz and I am a web developer with over 2 years of experience in the field. I specialize in front-end development and have a strong background in HTML, CSS, JavaScript, PHP, and WordPress. I am always looking to learn and grow as a developer, and I am excited to work on new and challenging projects."
+        }
+    }
+}
+
+</script>
 <template>
     <article id="about">
         <div class="about__left">
-            <h4 class="article__subheading">Who am I?</h4>
-            <h2 class="article__heading">I develop <span class="highlight__text article__heading">quality, accessible</span> websites.</h2>
+            <h4 class="article__subheading">{{ subheading }}</h4>
+            <h2 class="article__heading" v-html="heading"></h2>
         </div>
         <div class="about__right">
-            <p class="about__description">Hi, my name is Khawar Mehfooz and I am a web developer with over 2 years of experience in the field. I specialize in front-end development and have a strong background in HTML, CSS, JavaScript, PHP, and WordPress. I am always looking to learn and grow as a developer, and I am excited to work on new and challenging projects. </p>
+            <p class="about__description">{{ description }} </p>
         </div>
     </article>
 
