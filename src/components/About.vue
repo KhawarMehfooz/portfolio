0cf06476979743ff94f7e6468be5b95885783c09
Khawarmehfooz <khawarmehfooz565@gmail.com>
2023-07-28 18:05:29 +0500

    Update About Component and Add More Description

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,29 +4,34 @@ export default{
         return{
             subheading: "Who am I?",
             heading: "I develop <span class='gradient__text article__heading'>quality, accessible</span> websites.",
-            description: "Hi, my name is Khawar Mehfooz and I am a web developer with over 2 years of experience in the field. I specialize in front-end development and have a strong background in HTML, CSS, JavaScript, PHP, and WordPress. I am always looking to learn and grow as a developer, and I am excited to work on new and challenging projects."
+            description: "Hello, I'm Khawar Mehfooz from Kotli, Azad Kashmir, Pakistan. Currently, I am a third-year student of BS Software Engineering at the University of Kotli Azad Jammu and Kashmir.",
+            moreDescription: "I am passionate about Web Development, and since 2021, I have been working as a Freelance WordPress Developer on Fiverr. On the platform, I help clients with Website Development, Bug fixing, and Website Customization using page builders and custom scripting with HTML, CSS, JavaScript, and PHP.<br><br>My main expertise lies in Front-end development, and I have a good knowledge of HTML, CSS, and JavaScript. Additionally, I am actively learning Back-end web development with PHP. Although I'm relatively new to PHP, I have the knowledge to develop fully functional database-driven CRUD web applications.<br><br>As a web developer, I am always eager to learn and grow. I am excited to work on new and challenging projects, and I'm open to collaborating with like-minded individuals in the industry."
         }
     }
 }
 
 </script>
 <template>
-    <article id="about">
-        <div class="about__left">
-            <h4 class="article__subheading">{{ subheading }}</h4>
-            <h2 class="article__heading" v-html="heading"></h2>
-        </div>
-        <div class="about__right">
-            <p class="about__description">{{ description }} </p>
-        </div>
-    </article>
-
+        <article class="container">
+            <div id="about" class="">
+                <div class="about__left">
+                    <h4 class="article__subheading">{{ subheading }}</h4>
+                    <h2 class="article__heading" v-html="heading"></h2>
+                </div>
+                <div class="about__right">
+                    <p class="about__description" v-html="description"> </p>
+                </div>
+            </div>
+            <p class="about__description" v-html="moreDescription"></p>
+        </article>
 </template>
 <style>
+.container{
+    padding: 6rem 4rem;
+}
 #about{
     display: flex;
-    padding: 6rem 4rem;
-
+    margin-bottom: 1.5rem;
 }
 .about__left{
     width: 50%;
@@ -38,12 +43,15 @@ export default{
     color: var(--light-dark);
     line-height: 1.5;
     font-size: 1.5rem;
+    text-align: justify;
 }
 @media only screen and (max-width:850px){
+    .container{
+        padding: 3rem 1rem;
+    }
     #about{
         flex-direction: column;
-        padding:2rem 0;
-
+        padding:1rem 0;
     }
     .about__left,
     .about__right{
@@ -51,9 +59,6 @@ export default{
     }
     .about__left{
         margin-bottom: 1rem;
-    }
-    .about__right{
-        margin-bottom: 4rem;
-    }   
+    }  
 }
 </style>
