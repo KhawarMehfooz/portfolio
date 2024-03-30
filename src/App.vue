370241b308eb5153d92d7ccbf993ca433361e452
khawarmehfooz <khawarmehfooz565@gmail.com>
2024-03-30 05:05:44 +0500

    create new navbar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <script setup>
-import Navbar from './components/Navbar.vue';
-import Hero from './components/Hero.vue';
-import About from './components/About.vue';
-import Skills from './components/Skills.vue';
-import Projects from './components/Projects.vue'
-import Testinomial from './components/Testinomial.vue';
-import Footer from './components/Footer.vue';
+import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
+import About from "./components/About.vue";
+import Skills from "./components/Skills.vue";
+import Projects from "./components/Projects.vue";
+import Testinomial from "./components/Testinomial.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
-  <Navbar/>
-  <Hero/>
-  <About />
-  <Skills/>
-  <Projects/>
-  <Testinomial/>
-  <Footer/>
+  <div class="max-w-4xl mx-auto pb-6 md:pb-12">
+    <Navbar />
+    <Hero />
+    <About />
+    <Skills />
+    <Projects />
+    <Testinomial />
+    <Footer />
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
